b79771d0908212bf86af1bf67afeefbeaf9be829
dvsteja <vnktsaiteja@gmail.com>
2022-05-29 09:56:10 +0530

    pre-pre-final

--- scripts/starter.sh
+++ scripts/starter.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
-
-# rm -f nohup.out
-# nohup tcpdump -i eth0 -nn tcp -w inputs/input-tcpdump.txt &
-
+pip install requirements.txt
 tcpdump -i eth0 -Z root -nn tcp >> onlo_fl_n_0.txt &
-# Write tcpdump's PID to a file
 echo $! > /var/run/tcpdump.pid
-
 netstat -npt -c >> onlo_fl_n.txt &
 echo $! > /var/run/netstat.pid
-
-python executor.py
+python3 executor.py
