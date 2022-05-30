209e0e9f1b20f4c6380431ca4c66973434c0e3b5
dvsteja <vnktsaiteja@gmail.com>
2022-05-30 07:26:24 +0530

    final-commit

--- scripts/connvzn.py
+++ scripts/connvzn.py
@@ -20,31 +20,31 @@ try:
     os.remove("prc_fl_stat_znl.txt")
     os.remove("prc_fl_stat_znl.csv")
     os.remove("download.zip")
-    cmd = "python3 starter.py"
-    subprocess.Popen(cmd,shell=True)
+    # cmd = "sh starter.sh"
+    # subprocess.Popen(cmd,shell=True)
 except:
     pass
-
-with open('onlo_fl_n.txt') as onlo_fl_n:
+dr = "/var/www/html/scripts"
+with open(dr+'/onlo_fl_n.txt') as onlo_fl_n:
     onlo_fl_n_ls = onlo_fl_n.readlines()
 
 onlo_fl_n_las = set() 
 onlo_fl_n_hl = "Active Internet connections (w/o servers)"
 
-with open("prc_fl_stat_znl.txt","w") as lde_fle_ot:    
+with open(dr+"/prc_fl_stat_znl.txt","w") as lde_fle_ot:    
     for onlo_fl_n_l in onlo_fl_n_ls:
         if onlo_fl_n_l not in onlo_fl_n_las: 
             if onlo_fl_n_hl not in onlo_fl_n_l:
                 lde_fle_ot.write(onlo_fl_n_l)
                 onlo_fl_n_las.add(onlo_fl_n_l)
 
-with open("prc_fl_stat_znl.txt") as exc_l_fi_let:
+with open(dr+"/prc_fl_stat_znl.txt") as exc_l_fi_let:
     onlo_fl_n_pr_lns = exc_l_fi_let.readlines()
     
 headers_original = "Proto,Recv-Q,Send-Q,Local,Address,Foreign,Address,State,PID/Program,name"
 headers_modified = "Con,In,Out,Local,Foreign,State,PID/Program"
 
-with open("prc_fl_stat_znl.csv","w") as execoutfile:
+with open(dr+"/prc_fl_stat_znl.csv","w") as execoutfile:
     for onlo_fl_nprc_ln in onlo_fl_n_pr_lns:
         onlo_fl_n_tres_metl_crs_l_0 = cmpl_pat_1.findall(onlo_fl_nprc_ln)
         if onlo_fl_n_tres_metl_crs_l_0:
@@ -58,7 +58,7 @@ with open("prc_fl_stat_znl.csv","w") as execoutfile:
 
         execoutfile.write(onlo_fl_n_nvs_pois_l_5)
 
-n_df_nod_ref = pd.read_csv('prc_fl_stat_znl.csv')
+n_df_nod_ref = pd.read_csv(dr+'/prc_fl_stat_znl.csv')
 n_df_nod_ref_incoming = n_df_nod_ref[['Foreign']] [n_df_nod_ref['In'] != 0]
 n_df_nod_ref_outgoing = n_df_nod_ref[['Foreign']] [n_df_nod_ref['Out'] != 0]
 n_df_nod_ref_twoway = n_df_nod_ref[['Local','Foreign','PID/Program']] [(n_df_nod_ref.In == 0) & (n_df_nod_ref.Out == 0)]
@@ -67,34 +67,34 @@ n_df_nod_ref_twoway = n_df_nod_ref[['Local','Foreign','PID/Program']] [(n_df_nod
 n_df_nod_ref_twoway_html = r'<link rel="stylesheet" type="text/css" href="hetro-frame-slide.css" />' + '\n'
 n_df_nod_ref_twoway_html += n_df_nod_ref_twoway.to_html(index=False)
 
-n_df_nod_ref_incoming.to_csv(r'inprocess.txt',header=None, index=None,mode='a')
-n_df_nod_ref_outgoing.to_csv(r'outprocess.txt',header=None, index=None,mode='a')
+n_df_nod_ref_incoming.to_csv(dr+'/inprocess.txt',header=None, index=None,mode='a')
+n_df_nod_ref_outgoing.to_csv(dr+'/outprocess.txt',header=None, index=None,mode='a')
 
-with open('twowayprocess.html','w') as twowyfile:
+with open(dr+'/twowayprocess.html','w') as twowyfile:
     twowyfile.write(n_df_nod_ref_twoway_html)
 
 
 myip = subprocess.getstatusoutput('hostname -I')
-myip1 = str(myip)
+myip1 = str(myip[1])
 # myip = socket.gethostbyname(socket.gethostname())
-with open('myip.txt','w') as output_file:
+with open(dr+'/myip.txt','w') as output_file:
     output_file.write(myip1)
 
-with open('onlo_fl_n_0.txt') as onlod_sec_nfl:
+with open(dr+'/onlo_fl_n_0.txt') as onlod_sec_nfl:
     onlod_sec_nfl_ls = onlod_sec_nfl.readlines()
 
-with open("incoming.txt", "w") as output_file:
+with open(dr+"/incoming.txt", "w") as output_file:
     for onlod_sec_nfl_l in onlod_sec_nfl_ls:
         onlod_sec_nfl_ip_list = cmpl_pat_2.findall(onlod_sec_nfl_l)
-        if not onlod_sec_nfl_ip_list[1] == myip:
+        if not onlod_sec_nfl_ip_list[1] == myip1:
             output_file.write(onlod_sec_nfl_ip_list[1]+'\n')
-with open("outgoing.txt", "w") as output_file:
+with open(dr+"/outgoing.txt", "w") as output_file:
     for onlod_sec_nfl_l in onlod_sec_nfl_ls:
         onlod_sec_nfl_ip_list = cmpl_pat_2.findall(onlod_sec_nfl_l)
-        if not onlod_sec_nfl_ip_list[0] == myip:
+        if not onlod_sec_nfl_ip_list[0] == myip1:
             output_file.write(onlod_sec_nfl_ip_list[0]+'\n')
 
-with ZipFile('download.zip','w') as dn_load_fl:
+with ZipFile(dr+'/download.zip','w') as dn_load_fl:
     dn_load_fl.write('twowayprocess.html')
     dn_load_fl.write('incoming.txt')
     dn_load_fl.write('inprocess.txt')
--- scripts/executor.py
+++ scripts/executor.py
@@ -4,6 +4,9 @@ from crontab import CronTab
 
 cron = CronTab(user='root')
 
-job = cron.new(command='python connvzn.py')
+job = cron.new(command='python3 connvzn.py')
 job.minute.every(1)
 cron.write()
+
+# if this fails execute it manually by adding below line to crontab -e [1]
+# */1 * * * * /usr/bin/python3 /var/www/html/scripts/executor.py
